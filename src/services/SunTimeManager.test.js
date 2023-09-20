bcc616fd70d5f54ae931f8a83e89e856277cd631
pdn-abs <pdn@abstractit.in>
2023-09-20 16:53:39 +0530

    Completed test for isNotBefore and getDayOfTheWeek functions

--- src/services/SunTimeManager.test.js
+++ src/services/SunTimeManager.test.js
@@ -2,12 +2,12 @@ import SunCalc from 'suncalc';
 import SunTimeManager from './SunTimeManager';
 import * as collection from '@laufire/utils/collection';
 import * as dayjs from 'dayjs';
-import { rndValue } from '@laufire/utils/random';
 
 jest.mock('dayjs', () => jest.fn());
 
 describe('sunTimeManager', () => {
-	const { getCalculatedSunTimes, isNotBefore } = SunTimeManager;
+	const { getCalculatedSunTimes, isNotBefore, getDayOfTheWeek }
+	= SunTimeManager;
 
 	const date = Symbol('date');
 	const latitude = Symbol('latitude');
@@ -43,21 +43,98 @@ describe('sunTimeManager', () => {
 			.toHaveBeenCalledWith(sunTimes, ['sunrise', 'sunset']);
 		expect(result).toEqual({ ...data, sunrise, sunset });
 	});
-	test('isNotBefore', () => {
-		const isSame = jest.fn();
-		const isAfter = jest.fn();
-		const dateTime = { isSame, isAfter };
+	describe('isNotBefore', () => {
+		test('if isSame returns true & isAfter returns false', () => {
+			const isSame = jest.fn(() => true);
+			const isAfter = jest.fn(() => false);
+			const dateTime = { isSame, isAfter };
+			const sunrise = Symbol('sunrise');
+
+			const result = isNotBefore(dateTime, sunrise);
+
+			expect(isSame).toHaveBeenCalledWith(sunrise);
+			expect(isAfter).not.toHaveBeenCalledWith(sunrise);
+
+			expect(result).toEqual(true);
+		});
+		test('if isSame returns false & isAfter returns true', () => {
+			const isSame = jest.fn(() => false);
+			const isAfter = jest.fn(() => true);
+			const dateTime = { isSame, isAfter };
+			const sunrise = Symbol('sunrise');
+
+			const result = isNotBefore(dateTime, sunrise);
+
+			expect(isSame).toHaveBeenCalledWith(sunrise);
+			expect(isAfter).toHaveBeenCalledWith(sunrise);
+
+			expect(result).toEqual(true);
+		});
+		test('if isSame returns false & isAfter returns false', () => {
+			const isSame = jest.fn(() => false);
+			const isAfter = jest.fn(() => false);
+			const dateTime = { isSame, isAfter };
+			const sunrise = Symbol('sunrise');
+
+			const result = isNotBefore(dateTime, sunrise);
+
+			expect(isSame).toHaveBeenCalledWith(sunrise);
+			expect(isAfter).toHaveBeenCalledWith(sunrise);
+
+			expect(result).toEqual(false);
+		});
+	});
+	describe('getDayOfTheWeek', () => {
 		const sunrise = Symbol('sunrise');
-		const sameValue = rndValue([true, false]);
-		const afterValue = rndValue([true, false]);
+		const sunTimeData = { tZone, latitude, longitude, date, sunrise };
 
-		jest.spyOn(dateTime, 'isSame').mockReturnValue(sameValue);
-		jest.spyOn(dateTime, 'isAfter').mockReturnValue(afterValue);
+		test('if isNotBefore returns true', () => {
+			const dayOfTheWeek = Symbol('dayOfTheWeek');
+			const format = jest.fn();
+			const dateTime = { format };
 
-		const result = isNotBefore(dateTime, sunrise);
+			jest.spyOn(SunTimeManager, 'getCalculatedSunTimes')
+				.mockReturnValue(sunTimeData);
+			jest.spyOn(dayjs, 'default').mockReturnValue(dateTime);
+			jest.spyOn(SunTimeManager, 'isNotBefore').mockReturnValue(true);
+			format.mockReturnValue(dayOfTheWeek);
 
-		expect(dateTime.isSame).toHaveBeenCalledWith(sunrise);
-		// eslint-disable-next-line jest/no-conditional-expect
-		sameValue || expect(dateTime.isAfter).toHaveBeenCalledWith(sunrise);
+			const result = getDayOfTheWeek(
+				date, latitude, longitude, tZone
+			);
+
+			expect(SunTimeManager.getCalculatedSunTimes)
+				.toHaveBeenCalledWith({ date, latitude, longitude, tZone });
+			expect(dayjs.default).toHaveBeenCalledWith(date);
+			expect(SunTimeManager.isNotBefore)
+				.toHaveBeenCalledWith(dateTime, sunrise);
+
+			expect(result).toEqual(dayOfTheWeek);
+		});
+
+		test('if isNotBefore returns false', () => {
+			const dayOfTheWeek = Symbol('dayOfTheWeek');
+			const format = jest.fn();
+			const subtract = jest.fn(() => ({ format }));
+			const dateTime = { subtract };
+
+			jest.spyOn(SunTimeManager, 'getCalculatedSunTimes')
+				.mockReturnValue(sunTimeData);
+			jest.spyOn(dayjs, 'default').mockReturnValue(dateTime);
+			jest.spyOn(SunTimeManager, 'isNotBefore').mockReturnValue(false);
+			format.mockReturnValue(dayOfTheWeek);
+
+			const result = getDayOfTheWeek(
+				date, latitude, longitude, tZone
+			);
+
+			expect(SunTimeManager.getCalculatedSunTimes)
+				.toHaveBeenCalledWith({ date, latitude, longitude, tZone });
+			expect(dayjs.default).toHaveBeenCalledWith(date);
+			expect(SunTimeManager.isNotBefore)
+				.toHaveBeenCalledWith(dateTime, sunrise);
+
+			expect(result).toEqual(dayOfTheWeek);
+		});
 	});
 });
