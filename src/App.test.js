dd7a2372f626bbae72179756239d81117121dc8b
pdn-abs <pdn@abstractit.in>
2023-09-22 12:30:07 +0530

    Completed test for App.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import context from './core/context';
 import { React } from 'react';
 import App from './App';
+import * as DayOfTheWeek from './DayOfTheWeek';
 
 describe('App', () => {
 	test('renders the component appropriately', () => {
@@ -11,11 +12,16 @@ describe('App', () => {
 		expect(rootElement).toBeInTheDocument();
 	});
 	test('Displays the DayOfTheWeek', () => {
+		jest.spyOn(DayOfTheWeek, 'default')
+			.mockReturnValue(<div role="DayOfTheWeek"/>);
+
 		render(<App { ...context }/>);
 
 		context.config.testData.forEach((date, id) => {
 			expect(screen.getAllByRole('DayOfTheWeek')[id])
 				.toBeInTheDocument();
+			expect(DayOfTheWeek.default)
+				.toHaveBeenCalledWith({ ...context, data: date }, {});
 		});
 	});
 });
